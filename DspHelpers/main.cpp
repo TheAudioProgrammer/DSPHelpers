c0e96b35503bf5ff9a3a92cf2d7e96f586d666c7
Joshua Hodge <josh@theaudioprogrammer.com>
2020-07-25 14:45:35 +0100

    Initial Commit

---
+++ DspHelpers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DspHelpers
+//
+//  Created by Joshua Hodge on 25/07/2020.
+//  Copyright © 2020 The Audio Programmer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
